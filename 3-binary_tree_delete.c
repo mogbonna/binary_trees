6f0353e3936fc99721dc601b8bd6cdac68ea2f2c
mogbonna <mogbonna045@gmail.com>
2024-01-02 14:12:25 -0800

    Write a function that checks if a node is a leaf

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,6 +3,8 @@
 /**
  * binary_tree_delete - Deletes an entire binary tree
  * @tree: Pointer to the root node of the tree to delete
+ *
+ * Return: Nothing!
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
