1c25ff9d8db1ecdd307445a530f5881113918d91
mogbonna <mogbonna045@gmail.com>
2024-01-02 15:56:59 -0800

    corrected and updated task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t right_side, left_side;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_side = binary_tree_height(tree->left);
+	right_side = binary_tree_height(tree->right);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	if (left_side > right_side)
+		return (left_side + 1);
+	return (right_side + 1);
 }
