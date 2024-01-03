bc06d8a12656c6e00749d34b50510c7078eeb15c
mogbonna <mogbonna045@gmail.com>
2024-01-02 17:49:02 -0800

    Write a function that finds the lowest common ancestor of two nodes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,42 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_ancestor_helper - Helper function for finding the ancestor
- * @root: Pointer to the root of the binary tree
- * @first: Pointer to the first node
- * @second: Pointer to the second node
- *
- * Return: Pointer to the lowest common ancestor node, NULL if not found
+ * binary_trees_ancestor - function that checks an ancestor
+ * @first: First node
+ * @second: Second node
+ * Return: the node of the ancestor
  */
-const binary_tree_t *binary_tree_ancestor_helper(const binary_tree_t *root,
-		const binary_tree_t *first, const binary_tree_t *second)
-{
-	const binary_tree_t *left, *right;
 
-	if (root == NULL || root == first || root == second)
-		return (root);
-
-	left = binary_tree_ancestor_helper(root->left, first, second);
-	right = binary_tree_ancestor_helper(root->right, first, second);
-
-	if (left != NULL && right != NULL)
-		return (root);
-
-	return ((left != NULL) ? left : right);
-}
-
-/**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
- * @first: Pointer to the first node
- * @second: Pointer to the second node
- *
- * Return: Pointer to the lowest common ancestor node, NULL if not found
- */
-const binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	const binary_tree_t *second)
 {
-	if (first == NULL || second == NULL)
-		return (NULL);
+	binary_tree_t *left, *right;
 
-	return (binary_tree_ancestor_helper(first, first, second));
+	if (first == NULL || second == NULL)
+	{
+		return (NULL);
+	}
+	if (first == second)
+	{
+		return ((binary_tree_t *)first);
+	}
+
+	left = first->parent;
+	right = second->parent;
+
+	if (left == NULL || first == right || (!left->parent && right))
+	{
+		return (binary_trees_ancestor(first, right));
+	}
+	else if (right == NULL || left == second || (!right->parent && left))
+	{
+		return (binary_trees_ancestor(left, second));
+	}
+	return (binary_trees_ancestor(left, right));
 }
