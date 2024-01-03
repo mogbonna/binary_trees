876d8ab250bca9637f5fb0c8053d257c9c84b889
mogbonna <mogbonna045@gmail.com>
2024-01-02 16:08:57 -0800

    corrected and updated task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,11 +8,16 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	size_t leaves = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	count = 1 + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
-	return (count);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	leaves = binary_tree_leaves(tree->left)
+		+ binary_tree_leaves(tree->right);
+
+	return (leaves);
 }
