8ec318a481581ce5ca01a43127d40f9b70f87033
mogbonna <mogbonna045@gmail.com>
2024-01-04 08:41:46 -0800

    Write a function that checks if a binary tree is a valid Binary Search Tree

--- binary_trees.h
+++ binary_trees.h
@@ -54,4 +54,8 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 
+
+
+int is_bst_helper(const binary_tree_t *tree, int min, int max);
+
 #endif /* _BINARY_TREES_H_ */
