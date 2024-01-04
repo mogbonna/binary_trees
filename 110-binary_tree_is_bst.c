e6edf6dd3da9b6406c30cf6e19230efd5d0f3914
mogbonna <mogbonna045@gmail.com>
2024-01-04 08:46:32 -0800

    Write a function that checks if a binary tree is a valid Binary Search Tree

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -23,11 +23,11 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 int is_bst_helper(const binary_tree_t *tree, int min, int max)
 {
 	if (tree == NULL)
-		return 1;
+		return (1);
 
 	if (tree->n <= min || tree->n >= max)
-		return 0;
+		return (0);
 
 	return (is_bst_helper(tree->left, min, tree->n) &&
-	        is_bst_helper(tree->right, tree->n, max));
+			is_bst_helper(tree->right, tree->n, max));
 }
